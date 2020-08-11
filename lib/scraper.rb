5dce05598d2d98092d4ca04595d74d5a813d7926
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-11 01:04:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,12 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
+  
+  def get_courses
+    self.get_page.css(".post")
+  end 
+  
+  
     # doc.css(".post").each do |post|
     #   course = Course.new
     #   course.title = post.css("h2").text
